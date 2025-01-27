df267baa56199364439fc6cc8c8a20a89775d47a
Jenn Wang <58282647+JingyiWng@users.noreply.github.com>
2025-01-27 15:40:59 -0500

    Convert a snowpark df to pandas df

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import requests
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import pandas
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -29,8 +30,12 @@ st.write("The name on your smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
 # st.dataframe(data=my_dataframe, use_container_width=True)
+
+# Convert the Snowpark df to a pandas df, so we can use the loc function                                                                       
+pd_df = my_datagrame.to_pandas()
+st.dataframe(pd_df)                                                                
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5) 
 
